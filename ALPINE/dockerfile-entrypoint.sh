39e4562c852a552d55baf3816625e2505ae88a9a
Ramon Santos <ramonss.bque@gmail.com>
2024-11-29 12:11:56 -0300

    aulas sobre entrypoint e cmd

---
+++ ALPINE/dockerfile-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Iniciando o container"
+
+for in in 1 2 3 4 5; do
+    echo "Contador: $i"
+done
+
+echo "Processos realizados, serviço em execução"
+
+echo "Iniciando 5 s"
+
+sleep 5s
+
+echo "Terminando 5 s"
